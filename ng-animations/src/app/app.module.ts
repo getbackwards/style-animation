3219477c663b28775d5dcd3932cccccbc41aa03a
getbackwards <getbackwards@gmail.com>
2017-05-08 21:30:44 +1200

    component scoped styles tutorial

--- ng-animations/src/app/app.module.ts
+++ ng-animations/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 // import { GoalComponent } from './goal/goal.component';
 import { QuoteComponent } from './quote/quote.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // GoalComponent,
-    QuoteComponent
+    QuoteComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
