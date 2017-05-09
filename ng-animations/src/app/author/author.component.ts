1672e0c5dbd4699d936ef48d3d900d314cb710b9
getbackwards <getbackwards@gmail.com>
2017-05-09 12:58:44 +1200

    tiny bit done at work

--- ng-animations/src/app/author/author.component.ts
+++ ng-animations/src/app/author/author.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
-  styleUrls: ['./author.component.css']
+  //styleUrls: ['./author.component.css']
+  styles: [`
+    h1 {
+      margin: 0;
+      font-size: 12px;
+      color: #42ad32;
+    }
+  `]
 })
 export class AuthorComponent implements OnInit {
 
