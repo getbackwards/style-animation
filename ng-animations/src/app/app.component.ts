3c5944edf3ffc3635d1ecd4ba83b2a19a46b4178
getbackwards <getbackwards@gmail.com>
2017-05-18 13:37:32 +1200

    Using Angular Renderer to adjust CSS styles

--- ng-animations/src/app/app.component.ts
+++ ng-animations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isFavourite = false;
+  constructor(private renderer: Renderer2) {}
+  onShowboring(element: HTMLElement) {
+    // element.style.display = 'block';
+    this.renderer.setStyle(element, 'display', 'block');
+  }
 }
